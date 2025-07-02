4551d32b2be77730cf6281a3a5ad138357b358ac
Eyal Lantzman <eyal_work@hotmail.com>
2025-07-02 12:31:40 +0100

    improve action manager

--- services/src/services/internal/action_manager.py
+++ services/src/services/internal/action_manager.py
@@ -1,6 +1,8 @@
 import asyncio
 import concurrent
 import uuid
+import functools
+import time
 from pydantic import BaseModel, Field
 from typing import Optional, Any
 from collections.abc import Callable
@@ -18,20 +20,26 @@ class ActionResult(BaseModel):
     class Config:
         arbitrary_types_allowed = True
 
+@functools.total_ordering
 class _Runnable(BaseModel):
     id: str
     func: Callable
+    callback: Optional[Callable] = None
+    timestamp: Optional[int] = Field(default_factory=time.monotonic_ns)
     args: Optional[list] = Field(default_factory=list)
     kwargs : Optional[dict] = Field(default_factory=dict)
     result:ActionResult = Field(default_factory=ActionResult)
     task: Optional[asyncio.Task[ActionResult]] = None
 
+    def __lt__(self, other):
+        return self.timestamp < other.timestamp
+    
     class Config:
         arbitrary_types_allowed = True
     
 
 class ActionManager:
-    def __init__(self, concurrency:int = 1, max_size:int = 0):
+    def __init__(self, concurrency:int = 1, max_size:int = 0, force_new_loop:bool = False):
         """
         Initiatizes and Action Manager that is responsible to control the number of concurrent actions.
 
@@ -43,15 +51,22 @@ class ActionManager:
                             queue reaches max_size, until an action is finished running.
         """
         self.concurrency = concurrency
-        self.tasks = dict[str, asyncio.Task[ActionResult]]()
-        self.loop = asyncio.new_event_loop()
+        if force_new_loop:
+            self.loop = asyncio.new_event_loop()
+        else:
+            try:
+                self.loop = asyncio.get_running_loop()
+            except RuntimeError:
+                self.loop = asyncio.new_event_loop()
+
         self.loop.set_task_factory(asyncio.eager_task_factory)
         self.loop.set_default_executor(concurrent.futures.ThreadPoolExecutor(max_workers=concurrency))
         self.incoming = asyncio.queues.PriorityQueue[_Runnable](max_size)
+        self.outgoing = dict[str, asyncio.Task[ActionResult]]()
 
         self.looping = False
 
-    def schedule(self, func:Callable, *args, id:str=None, priority:int=10,  **kwargs) -> asyncio.Task[str]:
+    def schedule(self, func:Callable, *args, id:str=None, priority:int=10, callback=None, **kwargs) -> asyncio.Task[str]:
         """Schedule function call, and wait until scheduled.
 
         Args:
@@ -65,15 +80,16 @@ class ActionManager:
         Returns:
             asyncio.Task: Scheduling task, with and id of the execution task.
         """
-        runnable = _Runnable(func=func, args=args, kwargs=kwargs, id=id or func.__qualname__ + str(uuid.uuid4()))
+        runnable_id = id or func.__qualname__ + str(uuid.uuid4())
+        runnable = _Runnable(func=func, args=args, kwargs=kwargs, id=runnable_id, callback=callback)
         runnable.result = ActionResult()
 
         async def _run():
             await self.incoming.put((priority, runnable))
             return runnable.id
-        
+
         task = self.loop.create_task(_run(), name="enqueue")
-        self.tasks[runnable.id] = None
+        self.outgoing[runnable.id] = None
 
         return task
 
@@ -93,6 +109,8 @@ class ActionManager:
         finally:
             runnable.result.completed = True
             logger.debug("Completed task %s", runnable.id)
+            if runnable.callback is not None:
+                self.loop.call_soon(runnable.callback, runnable.result)
         return runnable.result
 
     async def _run(self):
@@ -100,7 +118,7 @@ class ActionManager:
             try:
                 priority, work = await self.incoming.get()
                 future = self.loop.run_in_executor(None, self._do_work, work)
-                self.tasks[work.id] = future
+                self.outgoing[work.id] = future
                 self.incoming.task_done()
             except asyncio.CancelledError as e:
                 raise
@@ -113,24 +131,72 @@ class ActionManager:
 
     def wait_for_empty_queue(self):
         while not self.incoming.empty():
-            asyncio.get_event_loop().run_until_complete(asyncio.sleep(1))
+            self.loop.run_until_complete(asyncio.sleep(1))
 
     def wait_for_completion(self, awaitable:asyncio.Task | asyncio.Future):
-        return self.loop.run_until_complete(awaitable)
+       return self.loop.run_until_complete(awaitable)
 
-    def wait_for_execution(self, id:str) -> ActionResult:
-        if id not in self.tasks:
-            raise ValueError("Id is not present in the queue: %s", id)
+    async def await_for_completion(self, awaitable:asyncio.Task | asyncio.Future):
+       return await awaitable
+
+    async def await_for_execution(self, id:str, keep_storing_result:bool=False) -> ActionResult:
+        if id not in self.outgoing:
+            return None
+          
+        missing = object()
+        # None will indigate that we have a key but the value is None, 
+        # and missing will indicate that the key is missing.
+        # This way we can avoid KeyError due to race conditions when
+        # called from multiple threads and popped the id in one of them.
+        try:
+            awaitable = self.outgoing.get(id, missing)
+            while awaitable is None and awaitable is not missing:
+                await asyncio.sleep(0)
+                awaitable = self.outgoing.get(id, missing)
+        except KeyError:
+            # Could be a race condition between the first IF and the get(id,...)
+            return None
         
-        while self.tasks[id] is None:
-            asyncio.get_event_loop().run_until_complete(asyncio.sleep(1))
+        if awaitable is None:
+            # Some other request popped it
+            return None
 
-        return self.wait_for_completion(self.tasks[id])
+        try:
+            return await self.await_for_completion(awaitable)
+        finally:
+            if not keep_storing_result:
+                self.outgoing.pop(id, None)
 
+    
+    @functools.lru_cache
+    def wait_for_execution(self, id:str, keep_storing_result:bool=False) -> ActionResult:
+        if id not in self.outgoing:
+            return None
+        
+        missing = object()
+        try:
+            # The future is set after the scheduling tasks completed 
+            # i.e. scheduled for execution
+            awaitable = self.outgoing.get(id, missing)
+            while awaitable is None and awaitable is not missing:
+                self.loop.run_until_complete(asyncio.sleep(1))
+                awaitable = self.outgoing.get(id, missing)
+        except KeyError:
+            # Could be a race condition between the first IF and the get(id,...)
+            return None
+        
+        if awaitable is None:
+            return None
+        
+        try:
+            return self.wait_for_completion(awaitable)
+        finally:
+            if not keep_storing_result:
+                self.outgoing.pop(id, None)
+        
     def stop(self, msg: Any|None = None) -> bool:
         if self.looping:
             self.looping = False
-            asyncio.runners._cancel_all_tasks(self.loop)
             cancelled = self.background_run.cancel(msg)
-            self.loop.stop()
+            self.loop.call_soon_threadsafe(asyncio.runners._cancel_all_tasks, self.loop)
             return cancelled
--- services/tests/unit_tests/test_action_manager.py
+++ services/tests/unit_tests/test_action_manager.py
@@ -1,9 +1,19 @@
 import pytest
+from unittest.mock import patch
 import time
+import asyncio
 from services.internal.action_manager import ActionManager, ActionResult
 import logging
 
 logger = logging.getLogger(__name__)
+
+@pytest.fixture
+def wait_for_completion_mock():
+    patcher = patch("services.internal.action_manager.ActionManager.wait_for_completion")
+    mock = patcher.start()
+    mock.return_value = ActionResult()
+    yield mock
+    patcher.stop()
 
 @pytest.fixture
 def under_test():
@@ -135,6 +145,101 @@ def test_schedule_with_kwargs(under_test:ActionManager):
     assert outcome.completed
     assert outcome.last_error is None
 
+def test_schedule_with_exception(under_test:ActionManager):
+    # Arrnage
+    def throw_exception():
+        raise Exception("boom")
+    
+    under_test.schedule(func=throw_exception, id="test1")
+
+    # Act
+    under_test.start()
+    under_test.wait_for_empty_queue()
+
+    outcome = under_test.wait_for_execution(id="test1")
+
+    # Assert
+    assert not outcome.succeeded
+    assert outcome.completed
+    assert not outcome.cancelled
+    assert str(outcome.last_error) == "boom"
+
+def test_schedule_with_priorities(under_test:ActionManager):
+    # Arrnage
+    counter = 0
+    def increment_and_return():
+        nonlocal counter
+        counter += 1
+        return counter
+    
+    under_test.schedule(func=increment_and_return, id="test1", priority=3)
+    under_test.schedule(func=increment_and_return, id="test2", priority=2)
+    under_test.schedule(func=increment_and_return, id="test3", priority=1)
+
+    # Act
+    under_test.start()
+    under_test.wait_for_empty_queue()
+
+    outcome1 = under_test.wait_for_execution(id="test1")
+    outcome2 = under_test.wait_for_execution(id="test2")
+    outcome3 = under_test.wait_for_execution(id="test3")
+
+    # Assert
+    assert outcome1.result == 3
+    assert outcome2.result == 2
+    assert outcome3.result == 1
+
+def test_schedule_with_provided_id(under_test:ActionManager):
+    # Arrnage
+    under_test.start()
+
+    # Act
+    under_test.schedule(func_with_return_value, id="test1")
+    under_test.schedule(func_with_return_value, id="test2")
+    under_test.wait_for_empty_queue()
+    outcome1 = under_test.wait_for_execution(id="test1", keep_storing_result=True)
+    outcome2 = under_test.wait_for_execution(id="test1", keep_storing_result=False)
+    outcome3 = under_test.wait_for_execution(id="test1")
+    outcome4 = under_test.wait_for_execution(id="test2")
+    # Assert
+    assert outcome1 is outcome2
+    assert outcome1.result == 42
+    assert outcome3 is None
+    assert outcome1 is not outcome4
+    assert outcome4.result == 42
+
+
+def test_schedule_with_callback(under_test:ActionManager):
+    # Arrnage
+    under_test.start()
+    result = None
+    def callback(action_result:ActionResult):
+        nonlocal result
+        result = action_result
+
+    # Act
+    under_test.schedule(func_with_return_value, id="test1", callback=callback)
+    under_test.wait_for_empty_queue()
+    outcome = under_test.wait_for_execution(id="test1")
+
+    # Assert
+    assert outcome is result
+    assert result.result == 42
+
+def test_schedule_with_provided_id_and_cached_retrieval(under_test:ActionManager, wait_for_completion_mock):
+    # Arrnage
+    under_test.start()
+
+    # Act
+    task = under_test.schedule(func_with_return_value, id="test1")
+    under_test.wait_for_empty_queue()
+    outcome1 = under_test.wait_for_execution(id="test1")
+    outcome2 = under_test.wait_for_execution(id="test1")
+
+    # Assert
+    wait_for_completion_mock.assert_called_once()
+    assert outcome1 is outcome2
+
 @pytest.mark.parametrize("threads", [2,3,5,10])
 def test_schedule_with_concurrency(threads):
     # Arrnage
@@ -167,5 +272,47 @@ def test_schedule_with_concurrency(threads):
             assert outcome.completed, id
             assert outcome.last_error is None, id
 
+    finally:
+        under_test.stop()
+
+@pytest.mark.asyncio
+async def test_schedule_with_concurrency_multiple_await_the_same_id():
+    # Arrnage
+    threads = 10
+    under_test = ActionManager(10)
+    def my_func():
+        time.sleep(1)
+    under_test.start()
+    tasks = dict()
+    try:
+        # Act
+        for i in range(threads):
+            task = under_test.schedule(my_func)
+            tasks[task] = await under_test.await_for_completion(task)
+
+        for scheduled, id in tasks.items():
+            assert not scheduled.cancelled()
+            assert scheduled.done()
+            futures = await asyncio.gather(
+                under_test.await_for_execution(id, keep_storing_result=True),
+                under_test.await_for_execution(id, keep_storing_result=True),
+            )
+            
+            for future in futures:
+                assert future.result == None, id
+                assert not future.cancelled, id
+                assert future.completed, id
+                assert future.last_error is None, id
+
+            future = await under_test.await_for_execution(id)
+            assert future.result == None, id
+            assert not future.cancelled, id
+            assert future.completed, id
+            assert future.last_error is None, id
+
+            future = await under_test.await_for_execution(id)
+            assert future is None
+
+        assert len(under_test.outgoing) == 0
     finally:
         under_test.stop()
